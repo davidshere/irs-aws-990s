7afffe681f8ef417a85d08d17974c819b5ea571c
davidshere <sheredlk@gmail.com>
2016-12-11 14:01:08 -0500

    Script works but breaks if there is a comma in a field. Trying JSON

--- refresh_index.py
+++ refresh_index.py
@@ -5,7 +5,7 @@ import sqlite3
 from boto.s3.connection import S3Connection
 
 BUCKET_NAME = 'irs-form-990'
-INDEX_KEY_PREFIX = 'index'
+INDEX_KEY_PREFIX = 'index_2014'
 CSV_EXTENSION = '.csv'
 
 INDEX_TABLE_NAME = 'return_index'
@@ -36,30 +36,32 @@ def create_or_truncate_return_index_table(conn):
 	else:
 		cur.execute(CREATE_TABLE_QUERY)
 
-def process_index_key(key):
-	""" Take a Key object and return a list of lists formatted for the SQLite3 DB """
-	data = key.read()
-	rows = data.split(b'\r\n')
-
-	# don't need the final newline or the header row
-	rows.pop()
-	rows.pop(0)
-	return rows
-
 def get_keys():
-	""" Fetch index CSV files from the S3 bucket and yield lists of index data objects """
+	""" Fetch index CSV files from the S3 bucket and return a list of Keys """
 	conn = S3Connection()
 	bucket = conn.get_bucket(BUCKET_NAME)
 	index_keys = [a for a in bucket.list(prefix=INDEX_KEY_PREFIX) if CSV_EXTENSION in a.name]
 	return index_keys
 
+def process_index_key(key):
+	""" Take a Key object and return a list of lists formatted for the SQLite3 DB """
 
-def write_index_data_to_db(list_of_rows, conn):
+	# the csv module needs a string, not bytes
+	data = key.read().decode()
+	buff = io.StringIO(data)
+	reader = csv.reader(buff)
+	rows = [row for row in reader]
+	import pdb
+	pdb.set_trace()
+	
+	# don't need the final newline or the header row
+	rows.pop(0)
+	return rows
+
+def write_index_data_to_db(index_rows, conn):
 	""" Take a list of lists and write each element (inner list) to the DB """
 	cur = conn.cursor()
-	split_rows = [row.split(b',') for row in list_of_rows]
-	cur.executemany(INSERT_QUERY, split_rows)
-	conn.commit()
+	cur.executemany(INSERT_QUERY, index_rows)
 
 if __name__ == "__main__":
 
@@ -70,10 +72,11 @@ if __name__ == "__main__":
 	create_or_truncate_return_index_table(dbconn)
 
 	# fetch the keys from S3 and write their contents to the database.
-	for key in get_keys():
+	keys = get_keys()
+	for key in keys:
 		print(key)
 		data = process_index_key(key)
 		write_index_data_to_db(data, dbconn)
 
 	# commit the changes
-	#dbconn.commit()
+	dbconn.commit()
