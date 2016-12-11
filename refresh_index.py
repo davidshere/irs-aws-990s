867815122fdb1601e5f8baa2dd6c94e53d26aecf
davidshere <sheredlk@gmail.com>
2016-12-11 17:04:34 -0500

    Update refresh index script to use JSON instead of CSV. It is slower but handles faulty data better

--- refresh_index.py
+++ refresh_index.py
@@ -1,46 +1,44 @@
 import csv
 import io
+import json
 import sqlite3
 
 from boto.s3.connection import S3Connection
 
 BUCKET_NAME = 'irs-form-990'
-INDEX_KEY_PREFIX = 'index_2014'
-CSV_EXTENSION = '.csv'
+INDEX_KEY_PREFIX = 'index'
+INDEX_FILE_EXTENSION = '.json'
 
-INDEX_TABLE_NAME = 'return_index'
+AWS_RESPONSE_JSON_FIELDS = ['DLN', 'OrganizationName', 'EIN', 'FormType', 'TaxPeriod', 'SubmittedOn', 'LastUpdated', 'ObjectId', 'URL']
 SQLITE_DB = 'index.db'
 
-CHECK_FOR_TABLE_QUERY = "SELECT name FROM sqlite_master WHERE type='table' AND name=?;"
 CREATE_TABLE_QUERY = '''
 	create table return_index (
-		return_id text,
-		filing text,
-		ein integer,
+		dln text,
+		org_name text,
+		ein text,
+		form_type text,
 		tax_period text,
 		sub_date text,
-		taxpayer_name text,
-		return_type text,
-		dln text,
-		object_id text
+		last_updated,
+		object_id text,
+		url text
 	);
 '''
-INSERT_QUERY = "insert into return_index (return_id, filing, ein, tax_period, sub_date, taxpayer_name, return_type, dln, object_id) values (?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
-def create_or_truncate_return_index_table(conn):
+INSERT_QUERY = "insert into return_index (dln, org_name, ein, form_type, tax_period, sub_date, last_updated, object_id, url) values (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+
+def create_return_index_table(conn):
 	""" Check for the existence of the return_index table in a SQLite3 DB """
 	cur = conn.cursor()
-	cur.execute(CHECK_FOR_TABLE_QUERY, [INDEX_TABLE_NAME])
-	if cur.fetchall():
-		cur.execute('delete from return_index')
-	else:
-		cur.execute(CREATE_TABLE_QUERY)
+	cur.execute('drop table if exists return_index')
+	cur.execute(CREATE_TABLE_QUERY)
 
 def get_keys():
 	""" Fetch index CSV files from the S3 bucket and return a list of Keys """
 	conn = S3Connection()
 	bucket = conn.get_bucket(BUCKET_NAME)
-	index_keys = [a for a in bucket.list(prefix=INDEX_KEY_PREFIX) if CSV_EXTENSION in a.name]
+	index_keys = [a for a in bucket.list(prefix=INDEX_KEY_PREFIX) if INDEX_FILE_EXTENSION in a.name]
 	return index_keys
 
 def process_index_key(key):
@@ -48,14 +46,12 @@ def process_index_key(key):
 
 	# the csv module needs a string, not bytes
 	data = key.read().decode()
-	buff = io.StringIO(data)
-	reader = csv.reader(buff)
-	rows = [row for row in reader]
-	import pdb
-	pdb.set_trace()
+	records = json.loads(data)
+	# the API returns a dictionary with a single key that maps to a list of dictionaries, so we need that key, to get the list
+	record_key = list(records.keys())[0]
+	rows = [[row[col] for col in AWS_RESPONSE_JSON_FIELDS] for row in records[record_key]]
+
 	
-	# don't need the final newline or the header row
-	rows.pop(0)
 	return rows
 
 def write_index_data_to_db(index_rows, conn):
@@ -68,8 +64,8 @@ if __name__ == "__main__":
 	# connect to the database
 	dbconn = sqlite3.connect(SQLITE_DB)
 
-	# create the table if it doesn't exist, truncate it if it does
-	create_or_truncate_return_index_table(dbconn)
+	# create the table, dropping an existing table if one exists
+	create_return_index_table(dbconn)
 
 	# fetch the keys from S3 and write their contents to the database.
 	keys = get_keys()
