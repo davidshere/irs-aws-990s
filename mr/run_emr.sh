3601bfeff9d4e1bc8661db81551ab209048f5542
davidshere <sheredlk@gmail.com>
2017-02-11 17:52:48 -0500

    Some more code for running on EMR

--- mr/run_emr.sh
+++ mr/run_emr.sh
@@ -1,3 +1,4 @@
 cp ../tax_return.py .
 
-python $1 -r emr s3://irs-form-990-hadoop/$2 --conf-path ~/.mrjob.conf --output-dir=s3://irs-form-990-hadoop/$3 --region='us-east-1' --num-core-instances=$4 --instance-type=m3.xlarge
+python $1 -r emr s3://irs-form-900-hadoop/$2 --conf-path ~/.mrjob.conf --output-dir=s3://irs-form-990-hadoop/$3 --region='us-east-1' 
+#python $1 -r emr s3://irs-form-990-hadoop/$2 --conf-path ~/.mrjob.conf --output-dir=s3://irs-form-990-hadoop/$3 --region='us-east-1' --num-core-instances=$4 --instance-type=m3.xlarge
