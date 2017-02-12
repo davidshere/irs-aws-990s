9db905856b7d176e7704f209dc94f871307e2df0
davidshere <sheredlk@gmail.com>
2017-02-12 16:39:40 -0500

    Add --paths command line option and read query paths into the class

--- mr/mr_select.py
+++ mr/mr_select.py
@@ -6,15 +6,23 @@ from tax_return import TaxReturn
 
 class MRGetFields(MRJob):
 
+    def mapper_init(self):
+        query_filename = self.options.paths
+        with open(query_filename, 'r') as f:
+            self.queries = f.read().split('\n')
+
     def mapper(self, _, line):
         tree = etree.XML(line)
         s = TaxReturn(tree)
         object_id = s.find('ObjectID')[0].text
-        yield object_id, 1
+        yield str(len((self.queries))), 1
         
-
     def reducer(self, key, values):
         yield key, sum(values)
+
+    def configure_options(self):
+        super(MRGetFields, self).configure_options()
+        self.add_file_option('--paths')
 
 
 if __name__ == '__main__':
